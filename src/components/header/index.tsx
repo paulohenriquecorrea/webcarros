56844dc061b0726518b4785d9f6d602f4e43b665
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-01-25 13:13:34 -0400

    feat:criado componente header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,35 @@
+import { Link } from 'react-router-dom';
+import {FiUser, FiLogIn} from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
 export function Header() {
+    const signed = false;
+    const loadingAuth = false;
+
     return (
-        <h1>Componente Header</h1>
+        <div className='w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4'>
+            <header className='flex w-full max-w-7xl items-center justify-between px-4 mx-auto'>
+                <Link to='/'>
+                    <img 
+                        src={logoImg}
+                        alt='Logo do site'
+                    />
+                </Link>
+
+                {!loadingAuth && signed && (
+                    <Link to="/dashboard">
+                        <div className='border-2 rounded-full p-1 border-gray-900'>
+                            <FiUser size={24} color="#000" />
+                        </div>
+                    </Link>
+                )}
+
+                {!loadingAuth && !signed && (
+                    <Link to="/login">
+                        <FiLogIn size={24} color="#000" />
+                    </Link>
+                )}
+            </header>
+        </div>
     )
 }
