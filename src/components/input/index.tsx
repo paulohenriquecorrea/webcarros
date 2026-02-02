81cb197ee68ff593950b553c83b530893e637b0c
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-02-02 01:29:46 -0400

    Criada validação do formulário de login com a bibliotea zod

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,18 +1,26 @@
+import type { RegisterOptions, UseFormRegister } from 'react-hook-form';
+
 interface InputProps {
     type: string;
     placeholder: string;
     name: string;
+    register: UseFormRegister<any>;
+    error?: string;
+    rules?: RegisterOptions;
 }
 
-export function Input({ type, placeholder, name }: InputProps) {
+export function Input({ type, placeholder, name, register, error, rules }: InputProps) {
     return (
         <div>
             <input 
                 className="w-full border-2 rounded-md h-11 px-2"
                 type={type}
                 placeholder={placeholder}
-                name={name}
+                {...register(name, rules)}
+                id={name}
             />
+
+            {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
         </div>
     )
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,8 +2,28 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import { Container } from '../../components/container';
 import { Input } from '../../components/input';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema = z.object({
+    email: z.string().email('Insira um email válido').nonempty('Campo email é obrigatório'),
+    password: z.string().nonempty('Campo senha é obrigatório'),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function Login() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+        resolver: zodResolver(schema),
+        mode: 'onChange',
+    });
+
+    function onSubmit(data: FormData) {
+        console.log(data);
+    }
+
     return(
         <Container>
             <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
@@ -16,12 +36,31 @@ export function Login() {
                 </Link>
                 <form
                     className='bg-white max-w-xl w-full rounded-lg'
+                    onSubmit={handleSubmit(onSubmit)}
                 >
-                    <Input 
-                        type="email"
-                        placeholder="Digite o seu email..."
-                        name="email"
-                    />
+                    <div className='mb-3'>
+                        <Input 
+                            type="email"
+                            placeholder="Digite o seu email..."
+                            name="email"
+                            error={errors.email?.message}
+                            register={register}
+                        />
+                    </div>
+
+                    <div className='mb-3'>
+                        <Input 
+                            type="password"
+                            placeholder="Digite sua senha..."
+                            name="password"
+                            error={errors.password?.message}
+                            register={register}
+                        />
+                    </div>
+
+                    <button>
+                        Acessar
+                    </button>
                 </form>
             </div>
         </Container>
